5a729b44d6d9b9d06c79843bd634b3b4b0b88740
Damith Senanayake <dracusds123@gmail.com>
2015-11-23 16:49:55 +0530

    changes merged for 4.4.3

--- core/org.wso2.carbon.user.core/src/main/java/org/wso2/carbon/user/core/common/AbstractUserStoreManager.java
+++ core/org.wso2.carbon.user.core/src/main/java/org/wso2/carbon/user/core/common/AbstractUserStoreManager.java
@@ -1184,7 +1184,9 @@ public abstract class AbstractUserStoreManager implements UserStoreManager {
             throw new UserStoreException(USER_NOT_FOUND + ": User " + userName + "does not exist in: "
                     + realmConfig.getUserStoreProperty(UserCoreConstants.RealmConfig.PROPERTY_DOMAIN_NAME));
         }
-
+        if(claims == null){
+            claims = new HashMap<>();
+        }
         // #################### <Listeners> #####################################################
         for (UserOperationEventListener listener : UMListenerServiceComponent
                 .getUserOperationEventListeners()) {
